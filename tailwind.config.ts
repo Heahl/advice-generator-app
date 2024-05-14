55ae0ab31c6af17a99455de0ca0e429da945629f
heahl <hendrik712.a@gmail.com>
2024-05-14 09:27:01 +0200

    final commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,20 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      colors: {
+        'light-cyan' : 'hsl(193, 38%, 86%)',
+        'neon-green' : 'hsl(150, 100%, 66%)',
+        'hover-neon-green' : 'hsl(150, 100%, 76%)',
+        'grayish-blue' : 'hsl(217, 19%, 38%)',
+        'dark-grayish-blue' : 'hsl(217, 19%, 24%)',
+        'dark-blue' : 'hsl(218, 23%, 16%)'
+      },
+      boxShadow: {
+        glow: '0 0 25px 5px hsl(150, 100%, 67%)', 
+      },
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        'manrope': ['Manrope', 'sans-serif'], 
       },
     },
   },
